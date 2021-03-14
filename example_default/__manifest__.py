c10a525e8f83e05aa8493b9869aa9b501546281f
Jorge Obiols <jorge.obiols@gmail.com>
2021-03-14 15:02:18 -0300

    []

---
+++ example_default/__manifest__.py
@@ -0,0 +1,33 @@
+    ##############################################################################
+    #    Copyright (C) 2021 jeo Software  (http://www.jeosoft.com.ar)
+    #    All Rights Reserved.
+    ##############################################################################
+{
+    'name': 'example',
+    'version': '13.0.1.0.0',
+    'category': 'Tools',
+    'summary': "Project example for v13 CE",
+    'author': "jeo Software",
+    'license': 'AGPL-3',
+    'depends': [
+        'sale_management',
+        'crm'
+    ],
+    'installable': True,
+    'application': False,
+
+    'env-ver': '2',
+    'config': [],
+    'port': '8069',
+
+    'git-repos': [
+        'https://github.com/jobiols/cl-example.git',
+        'https://github.com/ingadhoc/odoo-argentina.git',
+    ],
+
+    'docker-images': [
+        'odoo jobiols/odoo-jeo:13.0',
+        'postgres postgres:10.1-alpine',
+        'nginx nginx'
+    ]
+}
